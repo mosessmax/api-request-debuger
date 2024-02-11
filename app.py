60655c5f5fd521f775ffc08eeede45bdcbf28d95
adebayo-moses <adebayomoses597@gmail.com>
2024-02-11 01:50:53 +0100

    enhance error handling and logging in RequestLogger class

---
+++ app.py
@@ -0,0 +1,46 @@
+import requests
+import sys
+
+class RequestLogger:
+    def __init__(self):
+        self.log = []
+
+
+# try except block to handle the case where the response is not JSON
+    def request(self, method, url, **kwargs):
+        response = requests.request(method, url, **kwargs)
+        try:
+            response_json = response.json() if response.text else None
+        except ValueError:
+            response_json = None
+            self.log.append({'method': method, 'url': url, 'request': kwargs, 'response': response_json})
+        return response
+        
+    def get(self, url, **kwargs):
+        return self.request('GET', url, **kwargs)
+    
+    def post(self, url, **kwargs):
+        return self.request('POST', url, **kwargs)
+    
+    # def request(self, method, url, **kwargs):
+    #     response = requests.request(method, url, **kwargs)
+    #     self.log.append({'method': method, 'url': url, 'request': kwargs, 'response': response.json()})
+    #     return response
+
+    # def get(self, url, **kwargs):
+    #     return self.request('GET', url, **kwargs)
+
+    # def post(self, url, **kwargs):
+    #     return self.request('POST', url, **kwargs)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+         print("Error: Please provide a URL right after you type python app.py please..")
+         sys.exit(1)
+    
+    url = sys.argv[1]
+    print("preparing to boldly go where no Python script has gone before...")  #inserts dramatic music
+    logger = RequestLogger()
+    response = logger.get(url)
+    print("mission accomplished! Here's the log:")  #mission accomplished.
+    print(logger.log)
